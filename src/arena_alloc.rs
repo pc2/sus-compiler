b9c6f193574b10760fb4d0f9641857e9c48feed8
Lennart Van Hirtum <lennart.vanhirtum@gmail.com>
2023-10-04 23:44:55 +0200

    Added UUID based ListAllocator

--- src/arena_alloc.rs
+++ src/arena_alloc.rs
@@ -208,3 +208,100 @@ impl<'a, T, IndexMarker> IntoIterator for &'a mut ArenaVector<T, IndexMarker> {
         ArenaIteratorMut{it : self.data.iter_mut().enumerate(), _ph : PhantomData}
     }
 }
+
+
+pub struct ListAllocator<T, IndexMarker> {
+    data : Vec<T>,
+    _ph : PhantomData<IndexMarker>
+}
+
+impl<T, IndexMarker> ListAllocator<T, IndexMarker> {
+    pub fn new() -> Self {
+        Self{data : Vec::new(), _ph : PhantomData}
+    }
+    pub fn alloc(&mut self, v : T) -> UUID<IndexMarker> {
+        let uuid = UUID(self.data.len(), PhantomData);
+        self.data.push(v);
+        uuid
+    }
+    pub fn iter<'a>(&'a self) -> ListAllocIterator<'a, T, IndexMarker> {
+        self.into_iter()
+    }
+    pub fn iter_mut<'a>(&'a mut self) -> ListAllocIteratorMut<'a, T, IndexMarker> {
+        self.into_iter()
+    }
+}
+
+impl<T, IndexMarker> Index<UUID<IndexMarker>> for ListAllocator<T, IndexMarker> {
+    type Output = T;
+
+    fn index(&self, UUID(uuid, _): UUID<IndexMarker>) -> &Self::Output {
+        &self.data[uuid]
+    }
+}
+
+impl<T, IndexMarker> IndexMut<UUID<IndexMarker>> for ListAllocator<T, IndexMarker> {
+    fn index_mut(&mut self, UUID(uuid, _): UUID<IndexMarker>) -> &mut Self::Output {
+        &mut self.data[uuid]
+    }
+}
+
+pub struct ListAllocIterator<'a, T, IndexMarker> {
+    it: Enumerate<std::slice::Iter<'a, T>>,
+    _ph : PhantomData<IndexMarker>
+}
+
+impl<'a, T, IndexMarker> Iterator for ListAllocIterator<'a, T, IndexMarker> {
+    type Item = (UUID<IndexMarker>, &'a T);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.it.next() {
+            None => {
+                return None;
+            },
+            Some((pos, val)) => {
+                return Some((UUID(pos, PhantomData), val));
+            }
+        }
+    }
+}
+
+pub struct ListAllocIteratorMut<'a, T, IndexMarker> {
+    it: Enumerate<std::slice::IterMut<'a, T>>,
+    _ph : PhantomData<IndexMarker>
+}
+
+impl<'a, T, IndexMarker> Iterator for ListAllocIteratorMut<'a, T, IndexMarker> {
+    type Item = (UUID<IndexMarker>, &'a mut T);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.it.next() {
+            None => {
+                return None;
+            },
+            Some((pos, val)) => {
+                return Some((UUID(pos, PhantomData), val));
+            }
+        }
+    }
+}
+
+impl<'a, T, IndexMarker> IntoIterator for &'a ListAllocator<T, IndexMarker> {
+    type Item = (UUID<IndexMarker>, &'a T);
+
+    type IntoIter = ListAllocIterator<'a, T, IndexMarker>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        ListAllocIterator{it : self.data.iter().enumerate(), _ph : PhantomData}
+    }
+}
+
+impl<'a, T, IndexMarker> IntoIterator for &'a mut ListAllocator<T, IndexMarker> {
+    type Item = (UUID<IndexMarker>, &'a mut T);
+
+    type IntoIter = ListAllocIteratorMut<'a, T, IndexMarker>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        ListAllocIteratorMut{it : self.data.iter_mut().enumerate(), _ph : PhantomData}
+    }
+}
