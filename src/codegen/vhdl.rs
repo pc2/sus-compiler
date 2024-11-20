f8e46f2e79d8ebbfd699e605019c2f754026c7bd
Tobias Tom Hodapp <tobiastom2004@gmail.com>
2024-11-20 18:37:11 +0100

    Added types to port

--- src/codegen/vhdl.rs
+++ src/codegen/vhdl.rs
@@ -1,5 +1,11 @@
 use super::mangle;
-use crate::{linker::IsExtern, FlatAlloc, InstantiatedModule, Module, WireIDMarker};
+use crate::{
+    linker::{get_builtin_type, IsExtern},
+    typing::concrete_type::ConcreteType,
+    FlatAlloc, InstantiatedModule, Module, TypeUUID, WireIDMarker,
+};
+use std::fmt::Write;
+use std::ops::Deref;
 
 #[derive(Debug)]
 pub struct VHDLCodegenBackend;
@@ -24,6 +30,39 @@ struct CodeGenerationContext<'g, 'out, Stream: std::fmt::Write> {
     instance: &'g InstantiatedModule,
     program_text: &'out mut Stream,
     needed_untils: FlatAlloc<i64, WireIDMarker>,
+}
+
+fn get_type_name_size(id: TypeUUID) -> u64 {
+    if id == get_builtin_type("int") {
+        32 // TODO concrete int sizes
+    } else if id == get_builtin_type("bool") {
+        1
+    } else {
+        println!("TODO Named Structs Size");
+        1 // todo!() // Named structs are not implemented yet
+    }
+}
+
+fn typ_to_declaration(mut typ: &ConcreteType) -> String {
+    let mut array_string = String::new();
+    while let ConcreteType::Array(arr) = typ {
+        let (content_typ, size) = arr.deref();
+        let sz = size.unwrap_value().unwrap_integer();
+        write!(array_string, "array (0 to {}) of", sz - 1).unwrap();
+        typ = content_typ;
+    }
+    match typ {
+        ConcreteType::Named(id) => {
+            let sz = get_type_name_size(*id);
+            if sz == 1 {
+                format!("{array_string} std_logic")
+            } else {
+                format!("{array_string} std_logic_vector({} downto 0)", sz - 1)
+            }
+        }
+        ConcreteType::Array(_) => unreachable!("All arrays have been used up already"),
+        ConcreteType::Value(_) | ConcreteType::Unknown(_) => unreachable!(),
+    }
 }
 
 impl<'g, 'out, Stream: std::fmt::Write> CodeGenerationContext<'g, 'out, Stream> {
@@ -52,24 +91,32 @@ impl<'g, 'out, Stream: std::fmt::Write> CodeGenerationContext<'g, 'out, Stream> 
     fn write_module_signature(&mut self, commented_out: bool) {
         let comment_text = if commented_out { "-- " } else { "" };
         let instance_name = mangle(&self.instance.name);
+
+        let mut it = self.instance.interface_ports.iter_valids().peekable();
+        let end = if it.peek().is_some() { ";" } else { "" };
         write!(
             self.program_text,
-            "{comment_text}entity {} is (\n{comment_text}    port (\n",
+            "{comment_text}entity {} is (\n{comment_text}    port (\n        clk : in std_logic{end}\n",
             instance_name
         )
         .unwrap();
 
-        for (_, port) in self.instance.interface_ports.iter_valids() {
+        while let Some((_, port)) = it.next() {
             let port_wire = &self.instance.wires[port.wire];
-            let name = &port_wire.name;
-            let direction = if port.is_input { "in" } else { "out" };
-            dbg!(format!("{comment_text}       {name} : {direction}"));
+            let port_name = &port_wire.name;
+            let port_direction = if port.is_input { "in" } else { "out" };
+            let port_type = typ_to_declaration(&port_wire.typ);
+            let end = if it.peek().is_some() { ";" } else { "" };
+            write!(
+                self.program_text,
+                "{comment_text}        {port_name} : {port_direction}{port_type}{end}\n"
+            )
+            .unwrap();
         }
 
         write!(
             self.program_text,
-            "{comment_text}    ;\n{comment_text}end entity {}\n",
-            instance_name
+            "{comment_text}    );\n{comment_text}end entity {instance_name};\n"
         )
         .unwrap();
     }
