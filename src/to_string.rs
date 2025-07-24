cb9ce20763c9066aed758d666b2e39734b394351
Lennart Van Hirtum <lennart.vanhirtum@gmail.com>
2025-07-24 18:40:14 +0200

    Refactor to_string.rs to use FmtWrapper

--- src/to_string.rs
+++ src/to_string.rs
@@ -1,12 +1,13 @@
 use crate::alloc::zip_eq;
 use crate::prelude::*;
 
-use crate::typing::abstract_type::{AbstractGlobalReference, AbstractInnerType, PeanoType};
-use crate::typing::concrete_type::{ConcreteGlobalReference, ConcreteTemplateArg};
+use crate::typing::abstract_type::{AbstractGlobalReference, AbstractInnerType};
+use crate::typing::concrete_type::ConcreteGlobalReference;
 use crate::typing::domain_type::DomainType;
 use crate::typing::set_unifier::Unifyable;
 use crate::typing::template::{Parameter, TVec, TemplateKind};
-use crate::{file_position::FileText, pretty_print_many_spans, value::Value};
+use crate::value::Value;
+use crate::{file_position::FileText, pretty_print_many_spans};
 
 use crate::flattening::{
     Declaration, DeclarationKind, DomainInfo, InterfaceDeclKind, InterfaceDeclaration,
@@ -20,130 +21,22 @@ use std::fmt::{Display, Formatter};
 use std::fmt::Write;
 use std::ops::Deref;
 
-pub struct WrittenTypeDisplay<'a> {
-    inner: &'a WrittenType,
-    globals: &'a LinkerGlobals,
-    template_names: &'a TVec<Parameter>,
-}
-
-impl Display for WrittenTypeDisplay<'_> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self.inner {
-            WrittenType::Error(_) => f.write_str("{error}"),
-            WrittenType::TemplateVariable(_, id) => f.write_str(&self.template_names[*id].name),
-            WrittenType::Named(named_type) => {
-                f.write_str(&self.globals.types[named_type.id].link_info.get_full_name())
-            }
-            WrittenType::Array(_, sub) => {
-                write!(
-                    f,
-                    "{}[]",
-                    sub.deref().0.display(self.globals, self.template_names)
-                )
-            }
-        }
-    }
-}
-
 impl WrittenType {
     pub fn display<'a>(
         &'a self,
         globals: &'a LinkerGlobals,
         template_names: &'a TVec<Parameter>,
     ) -> impl Display + 'a {
-        WrittenTypeDisplay {
-            inner: self,
-            globals,
-            template_names,
-        }
-    }
-}
-
-pub struct AbstractRankedTypeDisplay<'a> {
-    typ: &'a AbstractRankedType,
-    globals: &'a LinkerGlobals,
-    link_info: &'a LinkInfo,
-}
-
-impl Display for AbstractRankedTypeDisplay<'_> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match &self.typ.inner {
-            AbstractInnerType::Unknown(_) => write!(f, "?"),
-            AbstractInnerType::Template(id) => {
-                f.write_str(&self.link_info.template_parameters[*id].name)
+        FmtWrapper(move |f| match self {
+            WrittenType::Error(_) => f.write_str("{error}"),
+            WrittenType::TemplateVariable(_, id) => f.write_str(&template_names[*id].name),
+            WrittenType::Named(named_type) => {
+                f.write_str(&globals.types[named_type.id].link_info.get_full_name())
             }
-            AbstractInnerType::Named(name) => f.write_fmt(format_args!(
-                "{}",
-                name.display(self.globals, self.link_info)
-            )),
-            AbstractInnerType::Interface(md_id, interface_id) => {
-                let md = &self.globals.modules[md_id.id];
-                f.write_fmt(format_args!(
-                    "Interface {} of {}",
-                    md.interfaces[*interface_id].name,
-                    md_id.display(self.globals, self.link_info)
-                ))
+            WrittenType::Array(_, sub) => {
+                write!(f, "{}[]", sub.deref().0.display(globals, template_names))
             }
-            AbstractInnerType::LocalInterface(local_interface) => f.write_fmt(format_args!(
-                "Local Interface '{}'",
-                self.link_info.instructions[*local_interface]
-                    .unwrap_interface()
-                    .name,
-            )),
-        }
-        .and_then(|_| f.write_fmt(format_args!("{}", &self.typ.rank)))
-    }
-}
-
-pub struct AbstractGlobalReferenceDisplay<'a, ID> {
-    typ: &'a AbstractGlobalReference<ID>,
-    globals: &'a LinkerGlobals,
-    link_info: &'a LinkInfo,
-}
-
-impl<ID: Into<GlobalUUID> + Copy> Display for AbstractGlobalReferenceDisplay<'_, ID> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let target_link_info: &LinkInfo = &self.globals[self.typ.id.into()];
-
-        f.write_str(&target_link_info.name)?;
-
-        if !self.typ.template_arg_types.iter().any(|(_, t)| match t {
-            TemplateKind::Type(_) => true,
-            TemplateKind::Value(_) => false,
-        }) {
-            return Ok(());
-        }
-
-        f.write_str(" #(")?;
-
-        let args_iter = zip_eq(
-            &self.typ.template_arg_types,
-            &target_link_info.template_parameters,
-        );
-
-        join_string_iter_formatter(", ", f, args_iter, |(_, typ, param), f| match typ {
-            TemplateKind::Type(typ) => f.write_fmt(format_args!(
-                "{}: {}",
-                param.name,
-                typ.display(self.globals, self.link_info)
-            )),
-            TemplateKind::Value(()) => f.write_fmt(format_args!("{}: _", param.name)),
-        })?;
-        f.write_str(")")
-    }
-}
-
-impl<ID: Into<GlobalUUID> + Copy> AbstractGlobalReference<ID> {
-    pub fn display<'a>(
-        &'a self,
-        globals: &'a LinkerGlobals,
-        link_info: &'a LinkInfo,
-    ) -> impl Display + 'a {
-        AbstractGlobalReferenceDisplay {
-            typ: self,
-            globals,
-            link_info,
-        }
+        })
     }
 }
 
@@ -153,25 +46,50 @@ impl AbstractRankedType {
         globals: &'a LinkerGlobals,
         link_info: &'a LinkInfo,
     ) -> impl Display + 'a {
-        AbstractRankedTypeDisplay {
-            typ: self,
-            globals,
-            link_info,
-        }
+        FmtWrapper(move |f| {
+            let res = match &self.inner {
+                AbstractInnerType::Unknown(_) => write!(f, "?"),
+                AbstractInnerType::Template(id) => {
+                    f.write_str(&link_info.template_parameters[*id].name)
+                }
+                AbstractInnerType::Named(name) => {
+                    f.write_fmt(format_args!("{}", name.display(globals, link_info)))
+                }
+                AbstractInnerType::Interface(md_id, interface_id) => {
+                    let md = &globals.modules[md_id.id];
+                    f.write_fmt(format_args!(
+                        "Interface {} of {}",
+                        md.interfaces[*interface_id].name,
+                        md_id.display(globals, link_info)
+                    ))
+                }
+                AbstractInnerType::LocalInterface(local_interface) => f.write_fmt(format_args!(
+                    "Local Interface '{}'",
+                    link_info.instructions[*local_interface]
+                        .unwrap_interface()
+                        .name,
+                )),
+            };
+            res?;
+            // Print PeanoType rank using its custom Display impl
+            write!(f, "{}", PeanoTypeDisplay(&self.rank))
+        })
     }
 }
 
-impl Display for PeanoType {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        let mut cur = self;
+// Helper wrapper for PeanoType display
+struct PeanoTypeDisplay<'a>(&'a crate::typing::abstract_type::PeanoType);
+impl<'a> std::fmt::Display for PeanoTypeDisplay<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut cur = self.0;
         loop {
             match cur {
-                PeanoType::Zero => return Ok(()),
-                PeanoType::Succ(inner) => {
+                crate::typing::abstract_type::PeanoType::Zero => return Ok(()),
+                crate::typing::abstract_type::PeanoType::Succ(inner) => {
                     f.write_str("[]")?;
                     cur = inner;
                 }
-                PeanoType::Unknown(_) => {
+                crate::typing::abstract_type::PeanoType::Unknown(_) => {
                     write!(f, "[...]")?;
                     return Ok(());
                 }
@@ -180,31 +98,36 @@ impl Display for PeanoType {
     }
 }
 
-pub struct ConcreteTypeDisplay<'a> {
-    inner: &'a ConcreteType,
-    globals: &'a LinkerGlobals,
-    use_newlines: bool,
-}
-
-impl Display for ConcreteTypeDisplay<'_> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self.inner {
-            ConcreteType::Named(global_ref) => ConcreteGlobalReferenceDisplay {
-                target_link_info: &self.globals.types[global_ref.id].link_info,
-                template_args: &global_ref.template_args,
-                globals: self.globals,
-                use_newlines: self.use_newlines,
+impl<ID: Into<GlobalUUID> + Copy> AbstractGlobalReference<ID> {
+    pub fn display<'a>(
+        &'a self,
+        globals: &'a LinkerGlobals,
+        link_info: &'a LinkInfo,
+    ) -> impl Display + 'a {
+        FmtWrapper(move |f| {
+            let target_link_info: &LinkInfo = &globals[self.id.into()];
+            f.write_str(&target_link_info.name)?;
+            if !self.template_arg_types.iter().any(|(_, t)| match t {
+                TemplateKind::Type(_) => true,
+                TemplateKind::Value(_) => false,
+            }) {
+                return Ok(());
             }
-            .fmt(f),
-            ConcreteType::Array(arr_box) => {
-                let (elem_typ, arr_size) = arr_box.deref();
-                write!(
-                    f,
-                    "{}[{arr_size}]",
-                    elem_typ.display(self.globals, self.use_newlines)
-                )
-            }
-        }
+            f.write_str(" #(")?;
+            let args_iter = zip_eq(
+                &self.template_arg_types,
+                &target_link_info.template_parameters,
+            );
+            join_string_iter_formatter(", ", f, args_iter, |(_, typ, param), f| match typ {
+                TemplateKind::Type(typ) => f.write_fmt(format_args!(
+                    "{}: {}",
+                    param.name,
+                    typ.display(globals, link_info)
+                )),
+                TemplateKind::Value(()) => f.write_fmt(format_args!("{}: _", param.name)),
+            })?;
+            f.write_str(")")
+        })
     }
 }
 
@@ -214,11 +137,74 @@ impl ConcreteType {
         globals: &'a LinkerGlobals,
         use_newlines: bool,
     ) -> impl Display + 'a {
-        ConcreteTypeDisplay {
-            inner: self,
-            globals,
-            use_newlines,
-        }
+        FmtWrapper(move |f| match self {
+            ConcreteType::Named(global_ref) => {
+                // Avoid ambiguity: call display() directly on ConcreteGlobalReference
+                ConcreteGlobalReference::display(global_ref, globals, use_newlines).fmt(f)
+            }
+            ConcreteType::Array(arr_box) => {
+                let (elem_typ, arr_size) = arr_box.deref();
+                write!(f, "{}[", elem_typ.display(globals, use_newlines))?;
+                // arr_size is Unifyable<Value, ...>, which implements Display for Unifyable, not Value
+                match arr_size {
+                    Unifyable::Set(val) => {
+                        // Value does not implement Display, so use Debug
+                        write!(f, "{val}")?;
+                    }
+                    Unifyable::Unknown(_) => {
+                        write!(f, "_")?;
+                    }
+                }
+                f.write_str("]")
+            }
+        })
+    }
+}
+
+impl<ID: Into<GlobalUUID> + Copy> ConcreteGlobalReference<ID> {
+    pub fn display<'v>(
+        &'v self,
+        globals: &'v LinkerGlobals,
+        use_newlines: bool,
+    ) -> impl Display + 'v {
+        let target_link_info = &globals[self.id.into()];
+        FmtWrapper(move |f| {
+            let nl = if use_newlines { "\n    " } else { "" };
+            assert!(self.template_args.len() == target_link_info.template_parameters.len());
+            let object_full_name = target_link_info.get_full_name();
+            f.write_str(&object_full_name)?;
+            if self.template_args.is_empty() {
+                return f.write_str(" #()");
+            } else {
+                f.write_str(" #(")?;
+            }
+            let mut is_first = true;
+            for (_id, arg, arg_in_target) in
+                zip_eq(&self.template_args, &target_link_info.template_parameters)
+            {
+                if !is_first {
+                    f.write_str(", ")?;
+                }
+                is_first = false;
+                f.write_fmt(format_args!("{nl}{}: ", arg_in_target.name))?;
+                match arg {
+                    TemplateKind::Type(typ_arg) => {
+                        f.write_fmt(format_args!(
+                            "type {}",
+                            typ_arg.display(globals, use_newlines)
+                        ))?;
+                    }
+                    TemplateKind::Value(v) => match v {
+                        Unifyable::Set(value) => write!(f, "{value}")?,
+                        Unifyable::Unknown(_) => f.write_str("/* Could not infer */")?,
+                    },
+                }
+            }
+            if use_newlines {
+                f.write_str("\n")?;
+            }
+            f.write_char(')')
+        })
     }
 }
 
@@ -237,60 +223,34 @@ impl Display for Value {
     }
 }
 
-pub struct PhysicalDomainDisplay<'a> {
-    domain: DomainID,
-    domains: &'a FlatAlloc<DomainInfo, DomainIDMarker>,
-}
-
-impl Display for PhysicalDomainDisplay<'_> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        if let Some(physical_domain) = self.domains.get(self.domain) {
-            f.write_fmt(format_args!("{{{}}}", physical_domain.name))
-        } else {
-            f.write_fmt(format_args!(
-                "{{unnamed domain {}}}",
-                self.domain.get_hidden_value()
-            ))
-        }
-    }
-}
-
 impl DomainID {
     pub fn display<'d>(
-        &self,
+        &'d self,
         domains: &'d FlatAlloc<DomainInfo, DomainIDMarker>,
-    ) -> PhysicalDomainDisplay<'d> {
-        PhysicalDomainDisplay {
-            domain: *self,
-            domains,
-        }
-    }
-}
-
-pub struct DomainDisplay<'a> {
-    domain: DomainType,
-    domains: &'a FlatAlloc<DomainInfo, DomainIDMarker>,
-}
-
-impl Display for DomainDisplay<'_> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self.domain {
-            DomainType::Generative => f.write_str("gen"),
-            DomainType::Physical(physical_id) => physical_id.display(self.domains).fmt(f),
-            DomainType::Unknown(_) => unreachable!(),
-        }
+    ) -> impl Display + 'd {
+        FmtWrapper(move |f| {
+            if let Some(physical_domain) = domains.get(*self) {
+                f.write_fmt(format_args!("{{{}}}", physical_domain.name))
+            } else {
+                f.write_fmt(format_args!(
+                    "{{unnamed domain {}}}",
+                    self.get_hidden_value()
+                ))
+            }
+        })
     }
 }
 
 impl DomainType {
     pub fn display<'d>(
-        &self,
+        &'d self,
         domains: &'d FlatAlloc<DomainInfo, DomainIDMarker>,
-    ) -> DomainDisplay<'d> {
-        DomainDisplay {
-            domain: *self,
-            domains,
-        }
+    ) -> impl Display + 'd {
+        FmtWrapper(move |f| match self {
+            DomainType::Generative => f.write_str("gen"),
+            DomainType::Physical(physical_id) => physical_id.display(domains).fmt(f),
+            DomainType::Unknown(_) => unreachable!(),
+        })
     }
 }
 
@@ -424,71 +384,6 @@ impl Module {
     }
 }
 
-pub struct ConcreteGlobalReferenceDisplay<'a> {
-    template_args: &'a TVec<ConcreteTemplateArg>,
-    target_link_info: &'a LinkInfo,
-    globals: &'a LinkerGlobals,
-    /// If there should be newlines: "\n", otherwise ""
-    use_newlines: bool,
-}
-
-impl<'a> Display for ConcreteGlobalReferenceDisplay<'a> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let nl = if self.use_newlines { "\n    " } else { "" };
-        assert!(self.template_args.len() == self.target_link_info.template_parameters.len());
-        let object_full_name = self.target_link_info.get_full_name();
-        f.write_str(&object_full_name)?;
-        if self.template_args.is_empty() {
-            return f.write_str(" #()");
-        } else {
-            f.write_str(" #(")?;
-        }
-
-        let mut is_first = true;
-        for (_id, arg, arg_in_target) in zip_eq(
-            self.template_args,
-            &self.target_link_info.template_parameters,
-        ) {
-            if !is_first {
-                f.write_str(", ")?;
-            }
-            is_first = false;
-            f.write_fmt(format_args!("{nl}{}: ", arg_in_target.name))?;
-            match arg {
-                TemplateKind::Type(typ_arg) => {
-                    f.write_fmt(format_args!(
-                        "type {}",
-                        typ_arg.display(self.globals, self.use_newlines)
-                    ))?;
-                }
-                TemplateKind::Value(v) => match v {
-                    Unifyable::Set(value) => f.write_fmt(format_args!("{value}"))?,
-                    Unifyable::Unknown(_) => f.write_str("/* Could not infer */")?,
-                },
-            }
-        }
-        if self.use_newlines {
-            f.write_str("\n")?;
-        }
-        f.write_char(')')
-    }
-}
-impl<ID: Into<GlobalUUID> + Copy> ConcreteGlobalReference<ID> {
-    pub fn display<'v>(
-        &'v self,
-        globals: &'v LinkerGlobals,
-        use_newlines: bool,
-    ) -> ConcreteGlobalReferenceDisplay<'v> {
-        let target_link_info = &globals[self.id.into()];
-        ConcreteGlobalReferenceDisplay {
-            template_args: &self.template_args,
-            target_link_info,
-            globals,
-            use_newlines,
-        }
-    }
-}
-
 pub fn join_string_iter_formatter<'fmt, T>(
     sep: &str,
     f: &mut Formatter<'fmt>,
@@ -525,14 +420,10 @@ pub fn trim_known_prefix<'a>(in_str: &'a str, prefix: &str) -> &'a str {
     &in_str[prefix.len()..]
 }
 
-struct FmtWrapper<F: Fn(&mut Formatter<'_>) -> std::fmt::Result>(F);
+pub struct FmtWrapper<F: Fn(&mut Formatter<'_>) -> std::fmt::Result>(pub F);
 
 impl<F: Fn(&mut Formatter<'_>) -> std::fmt::Result> Display for FmtWrapper<F> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         (self.0)(f)
     }
-}
-
-pub fn with_fmt(f: impl Fn(&mut Formatter<'_>) -> std::fmt::Result) -> impl Display {
-    FmtWrapper(f)
 }
